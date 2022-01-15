65d611aa1a8e8f7a32ff1a96d9c8bb557e8b86e2
tofusupreme <yunishbacon@gmail.com>
2022-01-15 18:06:12 +0900

    Fixed up the bookmark controller

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie, dependent: :destroy
   belongs_to :list, dependent: :destroy
   validates :comment, length: {minimum: 6}
+  # the movie_id is unique to the list_id
   validates :movie, uniqueness: {scope: :list}
 end
