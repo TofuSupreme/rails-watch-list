11b0b9a2aea5fc42eb2a8f14f9ec218fb7cb5167
tofusupreme <yunishbacon@gmail.com>
2022-01-21 17:02:44 +0900

    Broken delete bookmarks route and show html. FIX ME

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
   validates :title, length: {minimum: 7}, uniqueness: true, presence: true
-  validates :overview, length: {minimum: 7}, presence: true
+  validates :overview, length: {maximum: 20}, presence: true
 end
